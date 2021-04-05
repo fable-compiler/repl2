5d295d3e8937a098b20d99c16d0258f6afc7c30d
Robert Pickering <robert@sqreen.io>
2021-04-05 11:51:01 +0200

    description and credit

--- public/samples/games/ants.fs
+++ public/samples/games/ants.fs
@@ -1,4 +1,19 @@
-module Mario
+// F# Ant Colony Fable Edition
+
+// Ported from: https://github.com/robertpi/F--Ant-Colony/ which is a folk of: https://github.com/Rickasaurus/F--Ant-Colony
+
+// Original notice:
+
+//
+// This is Richard Minerich's F# Ant Colony Silverlight Ediiton
+// Visit my Blog at http://RichardMinerich.com
+// This code is free to be used for anything you like as long as I am properly acknowledged.
+//
+// The basic Silverlight used here is based on Phillip Trelford's Missile Command Example
+// http://www.trelford.com/blog/post/MissileCommand.aspx
+//
+
+module Ants
 
 open Fable.Core
 open Fable.Core.JsInterop
